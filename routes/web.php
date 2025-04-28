6cd1ecef4a86d3bb98c3f20a67de354ad63ee6e9
miguel salviano <miguelcf.tg@gmail.com>
2025-04-28 09:39:06 -0300

    feat: login form

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/main/{value}', [MainController::class, 'index']);
+
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout']);
