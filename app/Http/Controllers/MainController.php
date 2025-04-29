e75c8188987e3d8ba3975e7ef30de630cf582bd9
miguel salviano <miguelcf.tg@gmail.com>
2025-04-29 15:45:47 -0300

    feat: edit notes

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,6 +8,7 @@ use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
+use League\Uri\UriTemplate\Operator;
 
 class MainController extends Controller
 {
@@ -43,6 +44,7 @@ class MainController extends Controller
             ]
 
         );
+
         $id = session('user.id');
 
         $note = new Note();
@@ -57,7 +59,45 @@ class MainController extends Controller
     public function editNote($id)
     {
         $id = Operations::decryptId($id);
-        echo "estou editando a nota de id $id";
+
+        $note = Note::find($id);
+
+        return view('edit_note', ['note' => $note]);
+    }
+
+    public function editNoteSubmit(Request $request)
+    {
+        $request->validate(
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3|max:3000',
+            ],
+
+            [
+                'text_title.required' => 'O título é obrigatório',
+                'text_title.min' => 'O título deve ter no mínimo :min caracteres',
+                'text_title.max' => 'O título deve ter no máximo :max caracteres',
+
+                'text_note.required' => 'A nota é obrigatória',
+                'text_note.min' => 'A nota deve ter no mínimo :min caracteres',
+                'text_note.max' => 'A nota deve ter no máximo :max caracteres',
+            ]
+
+        );
+
+        if ($request->note_id == NULL) {
+            return redirect()->route('home');
+        }
+
+        $id = Operations::decryptId($request->note_id);
+
+        $note = Note::find($id);
+
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        return redirect()->route('home');
     }
 
     public function deleteNote($id)
